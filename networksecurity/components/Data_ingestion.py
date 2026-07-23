370ae4ef33f610bfa1ea81463c3a4327406d13a2
K Praveen Kumar <kkpraveen566@gmail.com>
2026-07-24 00:09:51 +0530

    updated code

--- networksecurity/components/Data_ingestion.py
+++ networksecurity/components/Data_ingestion.py
@@ -5,10 +5,48 @@ from networksecurity.logging.logger import logging
 
 from networksecurity.entity.config_entity import DataIngestionConfig
 
-
+import pandas as pd 
+import numpy as np
 import os
 import sys 
 import pymongo
 from typing import List
 from sklearn.model_selection import train_test_split
 
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+
+class DataIngestion:
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
+        
+        try:
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def export_collection_as_datafram(self):
+        """Read Data from MOngo DB Database that is the resons for this function in the codebase 
+        """
+        
+        try:
+            database_name=self.data_ingestion_config.database_name
+            collection_name=self.data_ingestion_config.collection_name
+            self.mogo_client=pymongo.MongoClient(MONGO_DB_URL)
+            collection=self.mongo_client[database_name][collection_name]
+            
+            df=pd.DataFrame(list(collection.find()))
+            if "_id" in df.columns.to_list():
+                df=df.drop(columns=["_id"],axis=1)
+            
+            df.replace({"na":np.nan},inplace=True)
+            return df    
+        except Exception as e:    
+            raise NetworkSecurityException     
+    
+    def initiate_data_ingestion(self):
+        try:
+            dataframe=self.export_collection_as_datafram()
+        except Exception as e:
+            raise NetworkSecurityException
