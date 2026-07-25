a43328eec3b2d9600d1bf56baeea7968bd14980d
K Praveen Kumar <kkpraveen566@gmail.com>
2026-07-25 08:56:01 +0530

    utils and datavaildation

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -14,6 +14,8 @@ FILE_NAME: str = "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+
 
 
 
@@ -27,4 +29,15 @@ DATA_INGESTION_DATABASE_NAME:str ="PRAVEEN"
 DATA_INGESTION_DIR_NAME:str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str="feature_store"
 DATA_INGESTION_INGESTED_DIR:str="ingested"
-DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2                
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2  
+
+"""
+Data Validation realted constant stat with DATA_VALIDATION VAR NAME
+
+"""
+
+DATA_VAILDATION_DIR_NAME:str  = "data_validation" 
+DATA_VAILDATION_VAILD_DIR:str = "validated"
+DATA_VALIDATION_INVAILD_DIR:str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str= "drift_report"     
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str= "report.yml"             
