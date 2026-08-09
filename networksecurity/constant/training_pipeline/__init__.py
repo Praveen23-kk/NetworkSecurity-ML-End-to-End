d30e64c3e86f134132f79e42f3783be959f55984
K Praveen Kumar <kkpraveen566@gmail.com>
2026-08-09 18:46:37 +0530

    Setup of data Transfomration in numpy and next for pkl

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -41,3 +41,21 @@ DATA_VAILDATION_VAILD_DIR:str = "validated"
 DATA_VALIDATION_INVAILD_DIR:str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str= "drift_report"     
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str= "report.yml"             
+
+
+
+"""Data Transformation related constant start with DATA_TRANFORMATION VAR NAME"""
+
+DATA_TRANSFORMATION_DIR_NAME:str="data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str = "transformed_object"
+
+
+##knn imputer class replace nan values missing value KKN and replace the nan value witht that 
+DATA_TRANSFORMATION_IMPUTER_PARAMS:dict ={
+    "missing_values": np.nan,
+    "n_neighbors":3,
+    "weights":"uniform",
+}
+    
+    
