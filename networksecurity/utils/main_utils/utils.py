97bc5d913776769d6a990748ecbbddc684737438
K Praveen Kumar <kkpraveen566@gmail.com>
2026-08-12 18:43:47 +0530

    update the bug and the erro fixed

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -27,9 +27,9 @@ def write_yaml_file(file_path:str,content:object, replace:bool = False)->None:
     
     
 def save_numpy_array_data(file_path:str, array:np.array):
-    
+
     try:
-        dir_path = os.dirname(file_path)
+        dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,"wb") as file_obj:
             np.save(file_obj,array)
